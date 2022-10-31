e091b9719b52e47a573297f47c8a15f9e6ffec67
Joris van Rheden <jvanrheden@bol.com>
2022-10-31 21:33:49 +0100

    apply gitignore

--- build/js/packages/browser_application-test/adapter-browser.js
+++
@@ -1,2 +0,0 @@
-require("/Users/jvanrheden/git/personal/kotlinjs/browser_application/build/js/packages_imported/kotlin-test-js-runner/1.6.20/kotlin-test-karma-runner.js")
-module.exports = require("/Users/jvanrheden/git/personal/kotlinjs/browser_application/build/js/packages/browser_application-test/kotlin/browser_application-test.js")
